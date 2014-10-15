5c88e17c0a7162fc35e9384e8e0d34370e946a70
Jian Chen <chenjian221@yahoo.com>
2014-10-15 18:47:39 -0400

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<title>
+		Hello
+	</title>
+	<body>
+		<form action ="handle.php" method = "post">
+		<p>User Name : <input type ="text" name = "un" size="20" maxlength="40"></p>
+		<p>Password : <input type ="text" name = "pw" size="40" maxlength="60"></p>
+		<p><input type="submit" name="submit" vaule = "Submit"></p>
+		</form>
+	</body>
+</html>
