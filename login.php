0ecdb42b576c3eb4c17f3bcc044e2b362d2e8d56
Stephen Gillen <stephenattech@gatech.edu>
2014-10-17 04:44:01 -0400

    Making some initial structural changes

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 		Hello
 	</title>
 	<body>
-		<form action ="handle.php" method = "post">
+		<form action ="index.php" method = "post">
 		<p>User Name : <input type ="text" name = "un" size="20" maxlength="40"></p>
 		<p>Password : <input type ="text" name = "pw" size="40" maxlength="60"></p>
 		<p><input type="submit" name="submit" value = "Submit"></p>
