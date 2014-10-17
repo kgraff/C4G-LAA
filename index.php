99d59c4df846d08c4287e230651e193479087888
Stephen Gillen <stephenattech@gatech.edu>
2014-10-17 02:09:15 -0400

    Corrected typo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<form action ="handle.php" method = "post">
 		<p>User Name : <input type ="text" name = "un" size="20" maxlength="40"></p>
 		<p>Password : <input type ="text" name = "pw" size="40" maxlength="60"></p>
-		<p><input type="submit" name="submit" vaule = "Submit"></p>
+		<p><input type="submit" name="submit" value = "Submit"></p>
 		</form>
 	</body>
 </html>
