29c4ea3fae372c9831b134b948e9ad9445bc2438
Stephen Gillen <stephenattech@gatech.edu>
2014-10-17 07:02:41 -0400

    Most of login page completed
    But index simply displays the username
    You need to have a MySQL database called users, with a table called
    users, with at least one <username, password HASH> pair stored, in your
    XAMPP for this to work. The hash I'm using now is just PHP's
    password_hash(password, PASSWORD_BCRYPT), but I think this is based on
    DES, so I'll have to go back and make it stronger.

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-
-
+session_start();
+$username = $_SESSION['username'];
+$password = $_SESSION['password'];
+if (!$username || !$password) {
+	header('Location: login.php');
+}
+echo $username;
 ?>
--- login.php
+++ login.php
@@ -1,12 +1,42 @@
-<html>
-	<title>
-		Hello
-	</title>
-	<body>
-		<form action ="index.php" method = "post">
-		<p>User Name : <input type ="text" name = "un" size="20" maxlength="40"></p>
-		<p>Password : <input type ="text" name = "pw" size="40" maxlength="60"></p>
-		<p><input type="submit" name="submit" value = "Submit"></p>
-		</form>
-	</body>
-</html>
+<?php
+session_start();
+if (array_key_exists('username', $_REQUEST) && array_key_exists('password', $_REQUEST)) {
+	$username = $_REQUEST['username'];
+	$password = $_REQUEST['password'];
+	if (! (mysql_connect('localhost','root',''))) {
+		die(mysql_error());
+	}
+	if (! (mysql_select_db('users'))) {
+		die(mysql_error());
+	}
+	$result = mysql_query('SELECT * FROM users WHERE username="'.$username.'"');
+	/* Still need to test for injection attacks! */
+	
+	$row = mysql_fetch_array($result);
+	$user = $row['username'];
+	if($username != $user) {
+		die("<br />Invalid username.<br /> ");
+	}
+	$hash = $row['password'];
+	/* Fix hash to be SHA-256; this will require changing database column width from 60 to 256 */
+	if (! (password_verify($password, $hash))) {
+		die("<br />Invalid password.<br /> ");
+	}
+	$_SESSION['username'] = $username;
+	$_SESSION['password'] = $password;
+	header('Location: index.php');
+} else {
+	echo '<html>'
+		.'<title>'
+			.'Hello'
+		.'</title>'
+		.'<body>'
+			.'<form action ="" method = "post">'
+				.'<p>User Name : <input type ="text" name = "username" size="20" maxlength="40"></p>'
+				.'<p>Password : <input type ="text" name = "password" size="40" maxlength="60"></p>'
+				.'<p><input type="submit" name="submit" value = "Submit"></p>'
+			.'</form>'
+		.'</body>'
+	.'</html>';
+}
+?>
