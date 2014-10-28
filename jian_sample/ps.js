b1c097f8be1f6798d77a9dbbb13cfa56fef64423
Jian Chen <chenjian221@yahoo.com>
2014-10-27 21:11:33 -0400

    Jian's sample

---
+++ jian_sample/ps.js
@@ -0,0 +1,16 @@
+function showPassword() {
+    
+    var key_attr = $('#key').attr('type');
+    
+    if(key_attr != 'text') {
+        
+        $('.checkbox').addClass('show');
+        $('#key').attr('type', 'text');
+        
+    } else {
+        
+        $('.checkbox').removeClass('show');
+        $('#key').attr('type', 'password');
+        
+    }
+}
