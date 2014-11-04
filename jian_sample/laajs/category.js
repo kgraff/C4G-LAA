6d52a7778ee92b2960846b5744c0bd11ca042f28
Perron Jones <eaglescb20@yahoo.com>
2014-11-04 14:40:08 -0500

    changes to add a category front end to allow validation, submission, and verification. Needs to be tested.

--- jian_sample/laajs/category.js
+++ jian_sample/laajs/category.js
@@ -6,69 +6,69 @@ $(document).ready(function() {
 });
 
 function populateCategoryList() {
-	$.ajax({
-		url : "http://localhost/laaRest/api/categories",
-		context : document.body,
-		async : false,
-		type : 'GET',
-		dataType : "jsonp",
-		success : function(data) {
-			console.log("Data Success");
-			console.log(data);
+	// Retrieve all categories
+	var promise = Category.readAll();
+	promise.success(function(data) {
+			console.log("attempt: " + data);
+			// iterate through each category returned and add it to the category list and
+			// populate the object containing the list of categories and their IDs
 			$.each(data, function(key, value) {
-				console.log(data[key]);
-				console.log(data[key].name);
-				categories[data[key].name] = data[key].ID;
+				categories[data[key].name] = data[key].ID; // Add the category name and ID pair
 				$('#categoryList').append("<option value='" + data[key].name + "'>");
 			});
-		},
-		error : function(jqXHR, textStatus, errorThrown) {
+	});
+	
+	promise.error(function(jqXHR, textStatus, errorThrown) {
 			console.log("Status: " + textStatus);
 			console.log("Error: " + errorThrown);
 			alert("Error pre-populating categories.");
-		}
 	});
 }
 
 
 function createCategory() {
-	console.log("createCategory()");
+	// validate the user input used to create a category
 	var valid = validateCategory();
 	if (valid == true) {
-		console.log("success:" + valid);
 		var parentCategory = $('#category').val();
-		var parentCategoryID = $('#category').val();
+		var parentCategoryID = categories[parentCategory];
 		var name = $('#name').val();
 		var description = $('#description').val();
 
-		var category = new Category(parentCategoryID, name, description);
-		/*
-		 var promise = Category.create();
-		 var msg = null;
+		var category = new Category(null, parentCategoryID, name, description);
+		
+		var promise = Category.create();
+		var msg = null;
 
-		 promise.success(function(data) {
-		 console.log("attempt: " + data)
-		 msg = data;
-		 });
-		 if (msg != null) {
-		 window.open("dashboard.html", "_self");
-		 } else {
-		 document.getElementById("same").innerHTML = "The email has already been in the database";
-		 }
-		 */
+		promise.success(function(data) {
+			console.log("attempt: " + data);
+			msg = data;
+		}); 
+
+		if (msg != null) {
+			//notify success (or failure to insert) and redirect to dashboard
+			if(msg == "Successfully inserted category!"){ // FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				alert(msg);
+				window.open("dashboard.html", "_self");
+			}
+			else{
+				alert("Failed to create category. Please try again later.");
+			}
+		} else {
+			//notify failure and prompt to try again later.
+			alert("Failed to create category. Please try again later.");
+		}
 	}
 	else{
-		console.log("error: " + valid);
-		$('#m1').text("error: " + valid);
+		$('#m1').text("error: " + valid); //Display the error to the user
 	}
 }
 
 
 function validateCategory() {
 	console.log("validateCategory()");
-	// Double check to ensure parent category is correct if one is chosen
-	//Get parent category
-	
+	// Check to ensure parent category is correct if one is chosen
+	// Get parent category	
 	var parentCategory = $('#category').val();
 	if(parentCategory != null && parentCategory != "" && categories[parentCategory] == undefined){
 		console.log("invalid parent category");
@@ -87,15 +87,16 @@ function validateCategory() {
 	if(categories[categoryName] != undefined){
 		return (false, "category already exists");
 	}
-	
 	return true;
 }
 
-function Category(parentCategoryID, name, description) {
+function Category(ID, parentCategoryID, name, description) {
+	this.ID = ID;
 	this.parentCategoryID = parentCategoryID;
 	this.name = name;
 	this.description = description;
 
+	// AJAX call to create a category
 	this.create = function() {
 		return $.ajax({
 			url : "http://localhost/laaRest/api/category",
@@ -111,16 +112,6 @@ function Category(parentCategoryID, name, description) {
 			success : function(data) {
 				console.log("Data Success");
 				console.log(data);
-				//Alert user of a successful or failed entry
-				if(data){
-					//notify success and redirect to dashboard
-					alert(data);
-					window.location = "./dashboard.html";
-				}
-				else{
-					//notify failure and prompt to try again later.
-					alert(data + "\nPlease try again later.");
-				}
 			},
 			error : function(jqXHR, textStatus, errorThrown) {
 				console.log("Status: " + textStatus);
@@ -128,4 +119,85 @@ function Category(parentCategoryID, name, description) {
 			}
 		});
 	};
+	
+	// AJAX call to read a category
+	this.read = function() {
+		return $.ajax({
+			url : "http://localhost/laaRest/api/category/id/" + this.ID,
+			context : document.body,
+			async : false,
+			type : 'GET',
+			dataType : "jsonp",
+			success : function(data) {
+				console.log("Data Success");
+				console.log(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log("Status: " + textStatus);
+				console.log("Error: " + errorThrown);
+			}
+		});
+	};
+	
+	// AJAX call to read all categories
+	this.readAll = function() {
+		return $.ajax({
+			url : "http://localhost/laaRest/api/categories",
+			context : document.body,
+			async : false,
+			type : 'GET',
+			dataType : "jsonp",
+			success : function(data) {
+				console.log("Data Success");
+				console.log(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log("Status: " + textStatus);
+				console.log("Error: " + errorThrown);
+			}
+		});
+	};
+	
+	// AJAX call to update a category
+	this.update = function() {
+		return $.ajax({
+			url : "http://localhost/laaRest/api/category/id/" + this.ID,
+			data : {
+				'categoryID' : this.parentCategoryID,
+				'name' : this.name,
+				'description' : this.description
+			},
+			context : document.body,
+			async : false,
+			type : 'POST',
+			dataType : "jsonp",
+			success : function(data) {
+				console.log("Data Success");
+				console.log(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log("Status: " + textStatus);
+				console.log("Error: " + errorThrown);
+			}
+		});
+	};
+	
+	// AJAX call to update a category
+	this.delete = function() {
+		return $.ajax({
+			url : "http://localhost/laaRest/api/category/id/" + this.ID,
+			context : document.body,
+			async : false,
+			type : 'DELETE',
+			dataType : "jsonp",
+			success : function(data) {
+				console.log("Data Success");
+				console.log(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log("Status: " + textStatus);
+				console.log("Error: " + errorThrown);
+			}
+		});
+	};	
 }
