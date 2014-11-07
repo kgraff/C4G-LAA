928f8cda504ae24efbf30441879a32d62ed8eae9
Perron Jones <eaglescb20@yahoo.com>
2014-11-06 22:57:47 -0500

    implemented the ui for deleting a category

--- jian_sample/laajs/category.js
+++ jian_sample/laajs/category.js
@@ -24,7 +24,7 @@ function populateCategoryList() {
 				
 				// Append to list of categories on edit page
 				$('#editCategoryList').append('<a href="#" class="list-group-item" categoryID="' + data[key].ID + '">' +
-				'<button type="button" class="btn btn-default btn-xs pull-right" style="margin-left: 10px;"><span class="glyphicon glyphicon-remove"></span></button>' +
+				'<button type="button" class="btn btn-default btn-xs pull-right" style="margin-left: 10px;" data-toggle="modal" data-target="#remove_category_modal" onclick="populateRemoveCategoryModal(this);"><span class="glyphicon glyphicon-remove"></span></button>' +
 				'<button type="button" class="btn btn-default btn-xs pull-right" data-toggle="modal" data-target="#edit_category_modal" onclick="populateEditCategoryModal(this);"><span class="glyphicon glyphicon-pencil"></span>' +
 				'</button>' + data[key].name + '</a>');
 			});
@@ -61,6 +61,28 @@ function populateEditCategoryModal(editButton){
 		alert("error editing the chosen category");
 		// close the modal
 		setTimeout( "$('#edit_category_modal').modal('hide');", 100 );
+	}
+}
+
+function populateRemoveCategoryModal(removeButton){
+	// The anchor containing the button element contains the categoryID
+	// Therefore, the parentNode must be retrieved
+	var categoryID = removeButton.parentNode.getAttribute("categoryID");
+	var chosenCategory = categoryByID[categoryID];
+	if(chosenCategory != undefined){
+		console.log("success");
+		
+		$('#removeCategoryName').text(chosenCategory.name);
+		
+		// Populate hidden category id input
+		$('#chosenCategoryID').val(categoryID);
+	}
+	else{
+		console.log("error removing the chosen category");
+		// close the modal or do something
+		alert("error removing the chosen category");
+		// close the modal
+		setTimeout( "$('#remove_category_modal').modal('hide');", 100 );
 	}
 }
 
@@ -141,9 +163,8 @@ function editCategory() {
 }
 
 function deleteCategory() {
-	var name = $('#name').val();
-	var description = $('#description').val();
-	var ID = categoryIdByName[name]; // Get the current ID
+	// Get the chosen category ID from hidden input
+	var ID = $('#chosenCategoryID').val();
 
 	var category = new Category(ID, null, null, null);
 	var promise = category.delete();
@@ -156,7 +177,7 @@ function deleteCategory() {
 
 	if (msg != null) {
 		//notify success (or failure to delete) and redirect to edit/remove category page
-		if(msg == "Successfully deleted category!"){ // FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		if(msg == "Successfully Deleted category!"){ // FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 			alert(msg);
 			window.open("edit_category.html", "_self");
 		}
